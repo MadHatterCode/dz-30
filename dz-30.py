6ead6e88e9a6cee8f52d520b84d4b3b1ba337235
Sergii <sadante@gmail.com>
2024-04-06 10:38:30 +0300

    dz-30 solution

---
+++ dz-30.py
@@ -0,0 +1,84 @@
+from exception import UserException
+
+
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"My name is {self.first_name} {self.last_name}, I am {self.gender.lower()}, {self.age} years old"
+
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{super().__str__()}, record book: {self.record_book}"
+
+
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) == 10:
+            raise UserException("The group is already at max capacity")
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student_to_remove = list(filter(lambda x: x.last_name == last_name, self.group))
+        if student_to_remove:
+            self.group.difference_update(student_to_remove)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+
+    def __str__(self):
+        all_students = "".join([f"{student.first_name} {student.last_name} " for student in self.group])
+        return f'Number:{self.number}\n {all_students} '
+
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st3 = Student('Male', 30, 'Steve1', 'Jobs', 'AN142')
+st4 = Student('Female', 25, 'Liza1', 'Taylor', 'AN145')
+st5 = Student('Male', 30, 'Steve2', 'Jobs', 'AN142')
+st6 = Student('Female', 25, 'Liza2', 'Taylor', 'AN145')
+st7 = Student('Male', 30, 'Steve3', 'Jobs', 'AN142')
+st8 = Student('Female', 25, 'Liza3', 'Taylor', 'AN145')
+st9 = Student('Male', 30, 'Steve4', 'Jobs', 'AN142')
+st10 = Student('Female', 25, 'Liza4', 'Taylor', 'AN145')
+st11 = Student('Male', 30, 'Steve5', 'Jobs', 'AN142')
+st12 = Student('Female', 25, 'Liza5', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+gr.add_student(st3)
+gr.add_student(st4)
+gr.add_student(st5)
+gr.add_student(st6)
+gr.add_student(st7)
+gr.add_student(st8)
+gr.add_student(st9)
+gr.add_student(st10)
+gr.add_student(st11)
+print(gr)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+
+gr.delete_student('Taylor')
+print(gr)
+
+gr.delete_student('Taylor')
---
+++ exception.py
@@ -0,0 +1,8 @@
+class UserException(Exception):
+
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+    def get_exception_message(self):
+        return self.message
